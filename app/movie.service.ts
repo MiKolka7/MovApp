f4165b7b00adf59ae94cb74c2e71c907b7a8a7a3
Коля Коваль <MiKolka7@users.noreply.github.com>
2017-03-19 11:19:09 +0200

    fixed request get movie, simplify service

--- app/movie.service.ts
+++ app/movie.service.ts
@@ -1,38 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Jsonp, URLSearchParams } from '@angular/http';
+import { Http, URLSearchParams } from '@angular/http';
 import { Movie } from './movie';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MovieService {
-    constructor(private jsonp: Jsonp) { }
+    constructor(private http: Http) { }
     private moviesUrl = 'https://api.themoviedb.org/3/';
+    
     findPopularMovies(popular: string) {
         const endPoint = 'movie/popular';
         let params = new URLSearchParams();
         params.set('api_key', '1fc4006c54ca8559696ec59eb395a72f');
         params.set('popular', popular);
-        params.set('format', 'json');
-        params.set('callback', 'JSONP_CALLBACK');
-        return this.jsonp
+        return this.http
             .get(this.moviesUrl + endPoint, { search: params })
-            .map(response => <Movie[]>response.json().themoviedb.movies.movie);
+            .map(response => <Movie[]>response.json().results);
     }
 
-    // findMovieByGenre(id: string) {
-    //     const endPoint = 'movie.get';
-    //     let params = new URLSearchParams();
-    //     params.set('key', '1fc4006c54ca8559696ec59eb395a72f');
-    //     params.set('id', id);
-    //     params.set('format', 'json');
-    //     params.set('callback', 'JSONP_CALLBACK');
-    //     console.log(id);
-    //     return this.jsonp
-    //         .get(this.moviesUrl + endPoint, { search: params })
-    //         .map(response => {
-    //             console.log(response.json().themoviedb.movie);
-    //             return response.json().themoviedb.movie
-    //         });
-    // }
+    findMovieByGenre(id: string) {
+        const endPoint = 'movie/' + id;
+        let params = new URLSearchParams();
+        params.set('api_key', '1fc4006c54ca8559696ec59eb395a72f');
+        return this.http
+            .get(this.moviesUrl + endPoint, { search: params })
+            .map(response => response.json());
+    }
 
 }
